e2cea399d690e19e5030408b727b962a3ca7c728
kobin11 <kobi.ram@gmail.com>
2023-12-08 13:28:22 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,51 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+import {MatListModule} from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+
+import {MatDividerModule} from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button'
+import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule } from '@angular/common/http';
+import { MatPaginatorModule } from '@angular/material/paginator';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatListModule,
+    MatToolbarModule,
+    MatListModule,
+   MatFormFieldModule, 
+   MatInputModule,
+    MatIconModule,
+    MatDividerModule, 
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatTableModule,
+    HttpClientModule,
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/slidebar/slidebar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slidebar',
+  templateUrl: './slidebar.component.html',
+  styleUrl: './slidebar.component.scss'
+})
+export class SlidebarComponent {
+  opened = true;
+
+  
+
+}
